58d5458be6eaa4cebaae91efe25f555a18c24537
advy99 <advy99@gmail.com>
2020-10-29 12:44:59 +0100

    codigo p2

---
+++ P2/src/codigo_generadores.cpp
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cstdlib>
+#include "codigo_generadores.h"
+
+//Genera un número uniformemente distribuido en el
+//intervalo [0,1) a partir de uno de los generadores
+//disponibles en C. Lo utiliza el generador de demanda
+double uniforme() {
+	int t = random();
+	double f = ((double)RAND_MAX+1.0);
+	return (double)t/f;
+}
+
+
+
+//Construye la tabla de búsqueda de
+//tamaño n para la distribución de
+//la demanda del apartado (a).
+double * construye_prop_a(int n) {
+	int i;
+	double* temp;
+
+	if ((temp = (double*) malloc(n*sizeof(double))) == NULL) {
+		fputs("Error reservando memoria para generador uniforme\n",stderr);
+		exit(1);
+	}
+
+	temp[0] = 1.0/n;
+
+	for (i=1;i<n;i++)
+		temp[i] = temp[i-1]+1.0/n;
+
+	return temp;
+}
+
+
+//Construye la tabla de búsqueda de
+//tamaño n para la distribución de
+//la demanda del apartado (b).
+
+double * construye_prop_b(int n) {
+	int i, max;
+	double* temp;
+
+	if ((temp = (double*) malloc(n*sizeof(double))) == NULL) {
+		fputs("Error reservando memoria para generador proporcional\n",stderr);
+		exit(1);
+	}
+
+	max = (n/2)*(n+1);
+	temp[0] = n*1.0/max;
+
+	for (i=1;i<n;i++)
+		temp[i] = temp[i-1]+(double)(n-i)/max;
+
+	return temp;
+}
+
+
+
+//Construye la tabla de búsqueda de
+//tamaño n para la distribución de
+//la demanda del apartado (c).
+double * construye_prop_c(int n) {
+	int i, max;
+	double* temp;
+
+	if ((temp = (double*) malloc(n*sizeof(double))) == NULL) {
+		fputs("Error reservando memoria para generador triangular\n",stderr);
+		exit(1);
+	}
+
+	max = n*n/4;
+	temp[0] = 0.0;
+
+	for (i=1;i<(n/2);i++)
+		temp[i] = temp[i-1]+(double)i/max;
+
+	for (i=(n/2);i<n;i++)
+		temp[i] = temp[i-1]+(double)(n-i)/max;
+
+	return temp;
+}
+
+
+
+
+// Genera un valor de la
+// distribución de la demanda codificada en tabla, por el
+// método de tablas de búsqueda.
+// tama es el tamaño de la tabla, 100 en nuestro caso particular
+int genera_demanda(double * tabla, int tama) {
+	int i;
+	double u = uniforme();
+	i = 0;
+
+	while((i<tama) && (u>=tabla[i]))
+		i++;
+
+	return i;
+}
---
+++ P2/src/quiosco.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include "codigo_generadores.h"
+
+
+int main (int argc, char ** argv) {
+
+	int x, y, veces, modificacion, z;
+
+	char distribucion;
+
+	if (argc != 6 && argc != 7) {
+		std::cerr << "Uso incorrecto." << std::endl;
+		std::cerr << "\t Uso: bin/quiosco <x> <y> <n_veces> <distribucion> <modificacion> <z>" << std::endl;
+
+		exit(-1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	veces = atoi(argv[3]);
+	distribucion = argv[4][0];
+	modificacion = atoi(argv[5]);
+
+	if (modificacion != 0)
+		z = atoi(argv[6]);
+
+	double * tablademanda;
+
+	int demanda;
+
+	double sum, sum2, ganancia, gananciaesperada, desviaciont;
+
+	for ( int s = 0; s <= 100; s++ ){
+		sum = sum2 = 0.0;
+		srand(time(NULL));
+
+		if ( distribucion == 'a' ) {
+			tablademanda = construye_prop_a(100);
+
+		} else if ( distribucion == 'b' ) {
+			tablademanda = construye_prop_b(100);
+
+		} else if ( distribucion == 'c' ) {
+			tablademanda = construye_prop_c(100);
+
+		} else {
+			std::cerr << "Distribucion incorrecta, debe ser a, b o c" << std::endl;
+			exit(-2);
+		}
+
+
+		for ( int i = 0; i < veces; i++ ) {
+			demanda = genera_demanda(tablademanda, 100);
+
+
+			if ( modificacion == 0 ) {
+				if (s > demanda)
+					ganancia = demanda * x - (s - demanda) * y;
+				else
+					ganancia = s * x;
+
+			} else if ( modificacion == 1 ) {
+				if ( s > demanda )
+					ganancia = demanda * x - z;
+				else
+					ganancia = s * x;
+
+			} else if ( modificacion == 2 ) {
+				if ( s > demanda )
+					ganancia = demanda * 4;
+				else
+					ganancia = s * x;
+
+			} else {
+				ganancia = 0;
+			}
+
+			sum += ganancia;
+			sum2 += ganancia * ganancia;
+
+		}
+
+		gananciaesperada = sum / veces;
+
+		desviaciont = sqrt( (sum2 - veces * gananciaesperada * gananciaesperada) / (veces-1) );
+
+
+		std::cout << s << " " << x << " " << y << " " << veces
+					 << " " << distribucion << " " << gananciaesperada
+					 << " " << desviaciont << std::endl;
+
+	}
+
+	return 0;
+
+}
