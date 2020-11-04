6bc7f2b11b0f4a7fbbcc76896d588c2fda8a495c
advy99 <advy99@gmail.com>
2020-11-04 17:53:10 +0100

    codigo prueba mejora 1

---
+++ P2/src/mejora1_generadores.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <chrono>
+#include "codigo_generadores.h"
+
+
+
+int main(int argc, char ** argv) {
+
+	if ( argc != 2 ) {
+		std::cerr << "Error - Uso: " << argv[0] << " <num_demandas> " << std::endl;
+		exit(-1);
+	}
+
+	int repeticiones = atoi(argv[1]);
+
+	tabla_demanda * tabla_demanda = construye_prop_c_ordenada(100);
+	double * tabla = construye_prop_c(100);
+	int basura;
+
+	auto start = std::chrono::steady_clock::now();
+
+	for (int i = 0; i < repeticiones; i++){
+		basura = genera_demanda(tabla, 100);
+	}
+
+	auto end = std::chrono::steady_clock::now();
+
+
+
+	std::chrono::duration<double> t_original = end - start;
+
+
+	start = std::chrono::steady_clock::now();
+
+	for (int i = 0; i < repeticiones; i++){
+		basura = genera_demanda(tabla_demanda, 100);
+	}
+
+	end = std::chrono::steady_clock::now();
+
+
+
+	std::chrono::duration<double> t_modificado = end - start;
+
+	std::cout << repeticiones << "\t" << t_original.count() << "\t" << t_modificado.count() << std::endl;
+
+	delete [] tabla_demanda;
+	delete [] tabla;
+
+
+	return 0;
+}
