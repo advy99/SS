28c16d756d80c8c75be45cca767e29efce685711
advy99 <advy99@gmail.com>
2020-11-04 17:48:15 +0100

    2.1: mejoras de los generadores

--- P2/include/codigo_generadores.h
+++ P2/include/codigo_generadores.h
@@ -1,11 +1,20 @@
 #ifndef CODGEN
 #define CODGEN
 
+struct tabla_demanda {
+	int posicion;
+	double prob_acumulada;
+};
+
 double uniforme();
 double* construye_prop_a(int n);
 double* construye_prop_b(int n);
 double* construye_prop_c(int n);
+tabla_demanda * construye_prop_c_ordenada(int n);
 int genera_demanda(double* tabla, int tama);
+int genera_demanda(tabla_demanda * tabla, int tama);
+int genera_demanda_a_mejorado(int tama);
+int genera_demanda_busqueda_binaria(double * tabla, int tama);
 
 #endif
 
--- P2/src/codigo_generadores.cpp
+++ P2/src/codigo_generadores.cpp
@@ -90,6 +90,52 @@ double * construye_prop_c(int n) {
 
 
 
+tabla_demanda * construye_prop_c_ordenada ( int n ) {
+	int i, max;
+	tabla_demanda * temp = new tabla_demanda[n];
+
+	if ( temp == nullptr ) {
+		std::cerr << "Error reservando memoria para generador uniforme" << std::endl;
+
+		exit(1);
+	}
+
+	max = n*n/4;
+
+	int valor_medio = n / 2;
+
+	int iterador = 0;
+	temp[iterador].posicion = valor_medio;
+	temp[iterador].prob_acumulada = (double)valor_medio / max;
+	iterador++;
+
+
+	for ( i = valor_medio - 1; i > 0; i-- ) {
+		double probabilidad = (double)i / max;
+
+		temp[iterador].posicion = i;
+		temp[iterador].prob_acumulada = temp[iterador - 1].prob_acumulada + probabilidad;
+		iterador++;
+
+
+		temp[iterador].posicion = valor_medio + (valor_medio - i);
+		temp[iterador].prob_acumulada = temp[iterador - 1].prob_acumulada + probabilidad;
+		iterador++;
+
+	}
+
+	if ( n % 2 == 0 ) {
+		double probabilidad = (double)0.0 / max;
+		temp[iterador].posicion = 0;
+		temp[iterador].prob_acumulada = temp[iterador - 1].prob_acumulada + probabilidad;
+	}
+
+
+	return temp;
+
+}
+
+
 
 // Genera un valor de la
 // distribución de la demanda codificada en tabla, por el
@@ -105,3 +151,55 @@ int genera_demanda(double * tabla, int tama) {
 
 	return i;
 }
+
+int genera_demanda_a_mejorado(int tama) {
+	double u = uniforme();
+	return u * tama;
+}
+
+int genera_demanda(tabla_demanda * tabla, int tama) {
+	int i;
+	double u = uniforme();
+	i = 0;
+
+	while((i<tama) && (u>=tabla[i].prob_acumulada))
+		i++;
+
+	return tabla[i].posicion;
+}
+
+
+// Genera un valor de la
+// distribución de la demanda codificada en tabla, por el
+// método de tablas de búsqueda.
+// tama es el tamaño de la tabla, 100 en nuestro caso particular
+int genera_demanda_busqueda_binaria(double * tabla, int tama) {
+	int i;
+	double u = uniforme();
+	int limite_superior = tama;
+	int limite_inferior = 0;
+
+	bool encontrado = false;
+
+
+	while ( !encontrado ) {
+		i = (limite_superior + limite_inferior) / 2;
+
+		if ( u < tabla[i] ) {
+			if ( i == 1 ) {
+				encontrado = true;
+			} else if (tabla[ i - 1 ] <= u) {
+				encontrado = true;
+			} else {
+				limite_superior = i - 1;
+			}
+		} else {
+			limite_inferior = i + 1;
+		}
+
+
+	}
+
+	return i;
+}
+
