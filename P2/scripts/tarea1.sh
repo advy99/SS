9c038f280929e3b741d73d5620d14c00f5b38379
advy99 <advy99@gmail.com>
2020-11-05 20:19:15 +0100

    generamos graficas tarea 1, modificacion 1

--- P2/scripts/tarea1.sh
+++ P2/scripts/tarea1.sh
@@ -4,7 +4,10 @@ mkdir -p ../datos
 mkdir -p ../datos/prob_a/
 mkdir -p ../datos/prob_b/
 mkdir -p ../datos/prob_c/
-mkdir -p ../graficos/
+mkdir -p ../graficas/
+mkdir -p ../graficas/prob_a/
+mkdir -p ../graficas/prob_b/
+mkdir -p ../graficas/prob_c/
 
 for distribucion in a b c; do
 	for veces in 100 1000 5000 10000 100000 150000; do
@@ -14,17 +17,19 @@ for distribucion in a b c; do
 	done
 done
 
-for modificacion in 1 2; do
-	for z in 1 5 10 15 20 25 30; do
+for distribucion in a b c; do
+	for modificacion in 1 2; do
+		for z in 10 50 100 500; do
 
-		for veces in 100 1000 5000 10000 100000 150000; do
-			for y in 1 5 10 15; do
-				../bin/quiosco 10 $y $veces $distribucion $modificacion $z > ../datos/prob_${distribucion}/datos_10_${y}_${veces}_${distribucion}_${modificacion}_${z}.dat
+			for veces in 100 1000 5000 10000 100000 150000; do
+				for y in 1 5 10 15; do
+					../bin/quiosco 10 $y $veces $distribucion $modificacion $z > ../datos/prob_${distribucion}/datos_10_${y}_${veces}_${distribucion}_${modificacion}_${z}.dat
+				done
 			done
+
+
 		done
 
-
 	done
-
 done
 
