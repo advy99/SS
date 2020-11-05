b041b837597b0a9b2c6d19d414dad30df850a20d
advy99 <advy99@gmail.com>
2020-11-05 17:41:09 +0100

    add todas las ejecuciones de quioscos

--- P2/scripts/tarea1.sh
+++ P2/scripts/tarea1.sh
@@ -13,4 +13,17 @@ for distribucion in a b c; do
 	done
 done
 
+for modificacion in 1 2; do
+	for z in 1 5 10 15 20 25 30; do
+
+		for veces in 100 1000 5000 10000 100000 500000; do
+			for y in 1 5 10 15; do
+				../bin/quiosco 10 $y $veces $distribucion $modificacion $z > ../datos/prob_${distribucion}/datos_10_${y}_${veces}_${distribucion}_${modificacion}_${z}.dat
+			done
+		done
+
+
+	done
+
+done
 
