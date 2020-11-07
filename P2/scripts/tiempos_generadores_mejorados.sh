8490ed5b6d0e0d4e332c5d893f0d77b658fc136f
advy99 <advy99@gmail.com>
2020-11-07 19:33:52 +0100

    arreglado script de medicion de tiempos

--- P2/scripts/tiempos_generadores_mejorados.sh
+++ P2/scripts/tiempos_generadores_mejorados.sh
@@ -3,15 +3,13 @@
 
 mkdir -p ../datos
 mkdir -p ../datos/tiempos_mejoras/
-mkdir -p ../datos/tiempos_mejoras/mejora1/
-mkdir -p ../datos/tiempos_mejoras/mejora2/
-mkdir -p ../datos/tiempos_mejoras/mejora3/
 mkdir -p ../graficas/
 
 
 for num_mejora in 1 2 3; do
-	for demanda in 100 500 1000 5000 10000 50000 100000 250000 500000 1000000; do
-		../bin/mejora${num_mejora}_generadores ${demanda} >> ../datos/tiempos_mejoras/mejora${num_mejora}/t_${num_mejora}_${demanda}.dat
+	echo "" > ../datos/tiempos_mejoras/t_${num_mejora}.dat
+	for demanda in $(seq 0 1000 1000000); do
+		../bin/mejora${num_mejora}_generadores ${demanda} >> ../datos/tiempos_mejoras/t_${num_mejora}.dat
 	done
 
 done
