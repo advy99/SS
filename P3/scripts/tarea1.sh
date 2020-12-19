ad82b3d9f1dae3ed0ea33e3dddc65d7c3af03c95
advy99 <advy99@gmail.com>
2020-12-19 23:30:40 +0100

    script ejecuciones apartado 1

---
+++ P3/scripts/tarea1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p ../datos
+mkdir -p ../datos/tarea1/
+
+TPARAR=10000
+
+for repuesto in 0 1; do
+
+	for trepar in 1 2 4 8; do
+
+		for tfallo in 1 2 4 8; do
+			../bin/sistema_tiempo_fijo $repuesto $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_fijo_${repuesto}_${tfallo}_${trepar}_${TPARAR}.dat
+			../bin/sistema_tiempo_variable $repuesto $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_fijo_${repuesto}_${tfallo}_${trepar}_${TPARAR}.dat
+		done
+
+	done
+
+done
+
+
+
+
