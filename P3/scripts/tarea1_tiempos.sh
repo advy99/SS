9b8ce8dace3376240f135956bb68b6065b5c8af6
advy99 <advy99@gmail.com>
2020-12-20 17:52:43 +0100

    acabamos tarea1_tiempos

--- P3/scripts/tarea1_tiempos.sh
+++ P3/scripts/tarea1_tiempos.sh
@@ -3,12 +3,14 @@
 mkdir -p ../datos
 mkdir -p ../datos/tarea1/
 
-printf "# N. Dias \t T. Fijo \t T. Variable\n" > ../datos/tarea1/tiempos.dat
+printf "# N. Dias \t T. Fijo \t T. Variable\n" > ../datos/tarea1/tiempos_fijo.dat
+printf "# N. Dias \t T. Fijo \t T. Variable\n" > ../datos/tarea1/tiempos_variable.dat
 
 for dias in $(seq 100 10000 10000000); do
-	printf "$dias \t" >> ../datos/tarea1/tiempos.dat
-	../bin/sistema_tiempo_fijo 1 2 2 ${dias} | tail -1 | awk '{printf $4 "\t" }' >> ../datos/tarea1/tiempos.dat
-	../bin/sistema_tiempo_variable 1 2 2 ${dias} | tail -1 | awk '{print $4}' >> ../datos/tarea1/tiempos.dat
+	printf "$dias \t" >> ../datos/tarea1/tiempos_fijo.dat
+	printf "$dias \t" >> ../datos/tarea1/tiempos_variable.dat
+	../bin/sistema_tiempo_fijo 1 2 2 ${dias} | tail -1 | awk '{print $4 }' >> ../datos/tarea1/tiempos_fijo.dat
+	../bin/sistema_tiempo_variable 1 2 2 ${dias} | tail -1 | awk '{print $4}' >> ../datos/tarea1/tiempos_variable.dat
 done
 
 
