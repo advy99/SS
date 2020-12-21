83fa8e887d4a87abc740cf21fafee414e56b760b
advy99 <advy99@gmail.com>
2020-12-21 23:58:11 +0100

    menos ejecuciones en tarea 1

--- P3/scripts/tarea1.sh
+++ P3/scripts/tarea1.sh
@@ -7,9 +7,9 @@ TPARAR=10000
 
 for repuesto in 0 1; do
 
-	for trepar in 1 2 4 8; do
+	for trepar in 0.5 2; do
 
-		for tfallo in 1 2 4 8; do
+		for tfallo in 1; do
 			../bin/sistema_tiempo_fijo $repuesto $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_fijo_${repuesto}_${tfallo}_${trepar}_${TPARAR}.dat
 			../bin/sistema_tiempo_variable $repuesto $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_fijo_${repuesto}_${tfallo}_${trepar}_${TPARAR}.dat
 		done
