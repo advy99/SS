0cc5a3249323502284c3cc54d67c360f3f6f3b9e
advy99 <advy99@gmail.com>
2020-12-21 23:00:37 +0100

    script para la parte 4 acabado

--- P3/scripts/tarea4.sh
+++ P3/scripts/tarea4.sh
@@ -3,23 +3,49 @@
 mkdir -p ../datos
 mkdir -p ../datos/tarea4/
 
-MEJOR_A=0
-MEJOR_B=0
 
+for n_simul in 1 5 10 25 50; do
 
-for i in $(seq 1 1 100); do
-	SALIDA_A=$(../bin/puerto 1 3 0 0.25 0 | grep -o "[0-9]\+.[0-9]\+" | head -1)
-	SALIDA_B=$(../bin/puerto 1 3 1 0.25 0 | grep -o "[0-9]\+.[0-9]\+" | head -1)
+	MEJOR_A=0
+	MEJOR_B=0
 
-	if [ $(echo "$SALIDA_A > $SALIDA_B" |bc -l) ]; then
-		MEJOR_A=$(($MEJOR_A + 1))
-	else
-		MEJOR_B=$(($MEJOR_B + 1))
-	fi
+	for i in $(seq 1 1 100); do
+		SALIDA_A=$(../bin/puerto $n_simul 3 1 0.25 0 | grep -o "[0-9]\+.[0-9]\+" | head -1)
+		SALIDA_B=$(../bin/puerto $n_simul 3 0 0.25 0 | grep -o "[0-9]\+.[0-9]\+" | head -1)
+
+		if [ $(echo "$SALIDA_A < $SALIDA_B" |bc -l) -eq 1 ]; then
+			MEJOR_A=$(($MEJOR_A + 1))
+		else
+			MEJOR_B=$(($MEJOR_B + 1))
+		fi
+
+	done
+
+	echo "Con $n_simul simulaciones el modelo A (original) es preferible el ${MEJOR_A}% de veces"
+	echo "Con $n_simul simulaciones el modelo B (no le afectan las tormentas) es preferible el ${MEJOR_B}% de veces"
 
 done
 
-echo "El modelo A es preferible el ${MEJOR_A}% de veces"
-echo "El modelo B es preferible el ${MEJOR_B}% de veces"
 
+# comparamos el A con el C
+for n_simul in 1 5 10 25 50; do
 
+	MEJOR_A=0
+	MEJOR_C=0
+
+	for i in $(seq 1 1 100); do
+		SALIDA_A=$(../bin/puerto $n_simul 3 1 0.25 0 | grep -o "[0-9]\+.[0-9]\+" | head -1)
+		SALIDA_C=$(../bin/puerto $n_simul 3 1 0.15 0 | grep -o "[0-9]\+.[0-9]\+" | head -1)
+
+		if [ $(echo "$SALIDA_A < $SALIDA_C" |bc -l) -eq 1 ]; then
+			MEJOR_A=$(($MEJOR_A + 1))
+		else
+			MEJOR_C=$(($MEJOR_C + 1))
+		fi
+
+	done
+
+	echo "Con $n_simul simulaciones el modelo A (original) es preferible el ${MEJOR_A}% de veces"
+	echo "Con $n_simul simulaciones el modelo C (remolcador más rápido) es preferible el ${MEJOR_C}% de veces"
+
+done
