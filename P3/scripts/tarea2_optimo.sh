3d560973cf5d49dd176618e649a077144e5c4d45
advy99 <advy99@gmail.com>
2020-12-22 15:52:55 +0100

    script para el optimo corregido

--- P3/scripts/tarea2_optimo.sh
+++ P3/scripts/tarea2_optimo.sh
@@ -7,10 +7,17 @@ MAQUINAS=4
 TREPAR=2
 TFALLO=1.5
 DIAS=365
+PORCENTAJE=90
 
 for repuestos in $(seq 1 1 10); do
 	for reparadores in $(seq 1 1 10); do
-		../bin/reparadores $MAQUINAS $repuestos $reparadores $TREPAR $TFALLO $DIAS > ../datos/tarea2/reparadores_${MAQUINAS}_${repuestos}_${reparadores}_${TREPAR}_${TFALLO}_${DIAS}.dat
+		SALIDA=$(../bin/reparadores $MAQUINAS $repuestos $reparadores $TREPAR $TFALLO $DIAS | grep -o "[0-9]\+.[0-9]\+" | tail -1)
+
+		if [ $(echo "$SALIDA < $PORCENTAJE" |bc -l) -eq 1 ]; then
+			echo "Con $repuestos repuestos y $reparadores reparadores se ha conseguido que el porcentaje total de fallos esté por debajo de un $SALIDA por ciento"
+
+		fi
+
 	done
 done
 
