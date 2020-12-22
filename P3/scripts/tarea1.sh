fce8c60001101fb6cfbedf0574fffefc9f7dc42d
advy99 <advy99@gmail.com>
2020-12-22 11:56:11 +0100

    acabado script tarea 1: separado en dias horas y minutos

--- P3/scripts/tarea1.sh
+++ P3/scripts/tarea1.sh
@@ -5,16 +5,33 @@ mkdir -p ../datos/tarea1/
 
 TPARAR=10000
 
+# dias
+for trepar in 0.5 2; do
 
-for trepar in 0.5 2 12 48 720 2880; do
-
-	for tfallo in 1 24 1440; do
+	for tfallo in 1; do
 		../bin/sistema_tiempo_fijo 1 $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_fijo_1_${tfallo}_${trepar}_${TPARAR}.dat
 		../bin/sistema_tiempo_variable 1 $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_variable_1_${tfallo}_${trepar}_${TPARAR}.dat
 	done
 
 done
 
+# horas
+for trepar in 12 48; do
 
+	for tfallo in 24; do
+		../bin/sistema_tiempo_fijo 1 $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_fijo_1_${tfallo}_${trepar}_${TPARAR}.dat
+		../bin/sistema_tiempo_variable 1 $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_variable_1_${tfallo}_${trepar}_${TPARAR}.dat
+	done
 
+done
+
+# minutos
+for trepar in 720 2880; do
+
+	for tfallo in 1440; do
+		../bin/sistema_tiempo_fijo 1 $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_fijo_1_${tfallo}_${trepar}_${TPARAR}.dat
+		../bin/sistema_tiempo_variable 1 $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_variable_1_${tfallo}_${trepar}_${TPARAR}.dat
+	done
+
+done
 
