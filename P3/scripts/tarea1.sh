6038c5a4de55469a45c821b29dbd1895bbf96966
advy99 <advy99@gmail.com>
2020-12-22 11:31:53 +0100

    modificado script tarea 1 para lo que nos piden

--- P3/scripts/tarea1.sh
+++ P3/scripts/tarea1.sh
@@ -5,15 +5,12 @@ mkdir -p ../datos/tarea1/
 
 TPARAR=10000
 
-for repuesto in 0 1; do
 
-	for trepar in 0.5 2; do
+for trepar in 0.5 2 12 48 720 2880; do
 
-		for tfallo in 1; do
-			../bin/sistema_tiempo_fijo $repuesto $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_fijo_${repuesto}_${tfallo}_${trepar}_${TPARAR}.dat
-			../bin/sistema_tiempo_variable $repuesto $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_fijo_${repuesto}_${tfallo}_${trepar}_${TPARAR}.dat
-		done
-
+	for tfallo in 1 24 1440; do
+		../bin/sistema_tiempo_fijo 1 $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_fijo_1_${tfallo}_${trepar}_${TPARAR}.dat
+		../bin/sistema_tiempo_variable 1 $tfallo $trepar $TPARAR > ../datos/tarea1/sistema_t_variable_1_${tfallo}_${trepar}_${TPARAR}.dat
 	done
 
 done
