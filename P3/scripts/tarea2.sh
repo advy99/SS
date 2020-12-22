f752ab8c803d1a274bb8be7a99c3d141d21b5a91
advy99 <advy99@gmail.com>
2020-12-22 15:25:18 +0100

    script para generar datos tarea 2 modificado: solo movemos reparadores, fijamos lo demas

--- P3/scripts/tarea2.sh
+++ P3/scripts/tarea2.sh
@@ -6,12 +6,10 @@ mkdir -p ../datos/tarea2/
 MAQUINAS=10
 REPUESTOS=2
 DIAS=365
+TREPAR=1.2
+TFALLO=1
 
 for reparadores in $(seq 1 1 10); do
-	for trepar in 1 2 4 6; do
-		for tfallo in 1 2 4 6; do
-			../bin/reparadores $MAQUINAS $REPUESTOS $reparadores $trepar $tfallo $DIAS > ../datos/tarea2/reparadores_${MAQUINAS}_${REPUESTOS}_${reparadores}_${trepar}_${tfallo}_${DIAS}.dat
-		done
-	done
+	../bin/reparadores $MAQUINAS $REPUESTOS $reparadores $TREPAR $TFALLO $DIAS > ../datos/tarea2/reparadores_${MAQUINAS}_${REPUESTOS}_${reparadores}_${DIAS}.dat
 done
 
