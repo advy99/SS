589ce2660ea539bbdb2f376ab00a5b5e112c335e
advy99 <advy99@gmail.com>
2021-01-01 18:56:47 +0100

    hacemos una clase en condiciones para el modelo

---
+++ P4/include/SIR.h
@@ -0,0 +1,50 @@
+#ifndef SIR_H
+#define SIR_H
+
+#include <iostream>
+#include <vector>
+
+struct estado{
+	unsigned long int infectados;
+	unsigned long int supceptibles;
+	unsigned long int recuperados;
+};
+
+class SIR {
+	private:
+
+		// a en el guion
+		double capacidad_infeccion_enfermedad;
+
+		// b en el guion
+		double tiempo_duracion_infeccion;
+
+		// intervalo calculo: dt en el guion
+		double intervalo_calculo;
+
+		double intervalo_comunicacion;
+		double t_inicio;
+		double t_fin;
+
+		estado estado_inicial;
+
+		std::vector<estado> estados;
+
+		double t_actual;
+
+
+	public:
+		SIR(const int argc, const char ** argv);
+
+		void integracion();
+		void one_step_runge_kutta();
+		void one_step_euler();
+		void derivacion();
+
+		friend std::ostream & operator << (std::ostream & os, const SIR * modelo);
+
+
+};
+
+
+#endif
