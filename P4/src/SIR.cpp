ede6a29a68143e01b3066210adc56b989820328e
advy99 <advy99@gmail.com>
2021-01-01 20:55:13 +0100

    algo de implementacion, aunque de momento muchas ideas y pseudocodigo y poca cosa funcional

--- P4/src/SIR.cpp
+++ P4/src/SIR.cpp
@@ -8,7 +8,7 @@ SIR::SIR(const int argc, const char ** argv){
 
 	if ( argc != 10) {
 		std::cerr << "Error: Número inesperado de argumentos." << std::endl
-					 << "\t Uso: " << argv[0] << " <a> <b> <dt> <dcomunicacion> <tinic> <tfin> <I0> <S0> <R0>" << std::endl;
+					 << "\t Uso: " << argv[0] << " <a> <b> <dt> <dcomunicacion> <tinic> <tfin> <I0> <S0> <R0> <f_paso>" << std::endl;
 		exit(-1);
 	}
 
@@ -24,6 +24,61 @@ SIR::SIR(const int argc, const char ** argv){
 
 	estados.push_back(estado_inicial);
 
+	std::string funcion_paso = std::string(argv[10]);
+
+	if ( funcion_paso == "euler" ){
+		puntero_funcion_paso = &SIR::one_step_euler;
+	} else {
+		puntero_funcion_paso = &SIR::one_step_runge_kutta;
+	}
+
+}
+
+
+
+void SIR::integracion() {
+	salida();
+
+	estado_anterior = estado;
+
+	one_step()
+}
+
+
+
+void SIR::simular() {
+	do {
+		integracion();
+		t_actual += intervalo_calculo;
+	} while ( t_actual < t_fin );
+}
+
+
+std::vector<double> SIR::one_step_runge_kutta(const std::vector<double> & estado){
+
+	std::vector<double> resultado = estado;
+
+	int tiempo = t_actual;
+	for ( int j = 0; j < 4; j++ ){
+		derivacion();
+
+		for ( int k = 0; k < 2; k++ ){
+
+		}
+
+	}
+
+	return resultado;
+}
+
+std::vector<double> SIR::derivacion(const std::vector<double> & estado){
+	std::vector<double> resultado = estado;
+
+	resultado[0] = capacidad_infeccion_enfermedad * estado[0] * estado[1] - tiempo_duracion_infeccion * estado[0];
+	resultado[1] = - capacidad_infeccion_enfermedad * estado[0] * estado[1];
+	resultado[2] = tiempo_duracion_infeccion * estado[0];
+
+	return resultado;
 }
 
 
