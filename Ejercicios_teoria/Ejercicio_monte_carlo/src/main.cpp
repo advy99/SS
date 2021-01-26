3b40d8c39aa61e0315b670394a5715ca4a422f38
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-01-26 13:03:39 +0100

    preparamos un fichero main

---
+++ Ejercicios_teoria/Ejercicio_monte_carlo/src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "planta_reciclaje.h"
+
+int main(int argc, char ** argv) {
+
+	if ( argc != 5 ) {
+		std::cerr << "ERROR: \n"
+					 << "Uso: " << argv[0] << " <num_dias> <kg_rojo> <kg_verde> <kg_reciclado_dia>"
+					 << std::endl;
+	}
+
+	int num_dias = atoi(argv[1]);
+	double kg_rojo = atof(argv[2]);
+	double kg_verde = atof(argv[3]);
+	double kg_reciclado_dia = atof(argv[4]);
+
+	PlantaReciclaje planta(num_dias, kg_rojo, kg_verde, kg_reciclado_dia);
+
+	planta.simular();
+
+}
