e790b4bebf7e867959dfb46564b5d378bb6deb3e
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-01-26 21:34:54 +0100

    añadimos el numero de veces, de momento temporal

--- Ejercicios_teoria/Ejercicio_monte_carlo/scripts/ejecuciones.sh
+++ Ejercicios_teoria/Ejercicio_monte_carlo/scripts/ejecuciones.sh
@@ -5,11 +5,11 @@ mkdir -p ../datos
 DIAS=365
 C_ROJO=300
 C_VERDE=0
-KG_PARA_RECICLAR_1=3
+KG_PARA_RECICLAR_1=300
 
 # Ejecución por defecto, sacamos la salida a un fichero de datos
 # y la salida de error nos mostrará en la terminal si hay algún punto
 # donde se llenan los contenedores
 printf "# Dia actual \t Contenedor rojo \t Contenedor Verde \t Papel usado perdido (sin espacio en rojo) \t Papel reciclado perdido (sin espacio en verde) \t Falta papel vender (sin papel reciclado)\n" > ../datos/ejecucion_defecto.dat
 
-../bin/main_reciclar $DIAS $C_ROJO $C_VERDE $KG_PARA_RECICLAR_1 >> ../datos/ejecucion_defecto.dat
+../bin/main_reciclar $DIAS $C_ROJO $C_VERDE $KG_PARA_RECICLAR_1 1 >> ../datos/ejecucion_defecto.dat
