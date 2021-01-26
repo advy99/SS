10a0a825ba37fb6715e9ce5f4c73b6978ca1060b
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-01-26 20:13:20 +0100

    guardamos los datos nuevos

--- Ejercicios_teoria/Ejercicio_monte_carlo/scripts/ejecuciones.sh
+++ Ejercicios_teoria/Ejercicio_monte_carlo/scripts/ejecuciones.sh
@@ -10,4 +10,6 @@ KG_PARA_RECICLAR_1=3
 # Ejecución por defecto, sacamos la salida a un fichero de datos
 # y la salida de error nos mostrará en la terminal si hay algún punto
 # donde se llenan los contenedores
-../bin/main_reciclar $DIAS $C_ROJO $C_VERDE $KG_PARA_RECICLAR_1 > ../datos/ejecucion_defecto.dat
+printf "# Dia actual \t Contenedor rojo \t Contenedor Verde \t Papel usado perdido (sin espacio en rojo) \t Papel reciclado perdido (sin espacio en verde) \t Falta papel vender (sin papel reciclado)\n" > ../datos/ejecucion_defecto.dat
+
+../bin/main_reciclar $DIAS $C_ROJO $C_VERDE $KG_PARA_RECICLAR_1 >> ../datos/ejecucion_defecto.dat
