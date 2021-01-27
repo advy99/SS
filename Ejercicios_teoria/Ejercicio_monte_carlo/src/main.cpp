0601954d01c8360f606286fd2cb76eaad1cb2564
advy99 <advy99@gmail.com>
2021-01-27 17:47:15 +0100

    main para repetir n_veces

--- Ejercicios_teoria/Ejercicio_monte_carlo/src/main.cpp
+++ Ejercicios_teoria/Ejercicio_monte_carlo/src/main.cpp
@@ -23,5 +23,24 @@ int main(int argc, char ** argv) {
 	planta.simular(true, true);
 
 
+	double media_papel_vendido = 0.0;
+	double media_papel_reciclado_perdido = 0.0;
+	double media_papel_usado_perdido = 0.0;
+	double media_falta_papel_reciclado = 0.0;
+
+	for ( int i = 0; i < n_veces; i++ ) {
+		planta.simular(false, false);
+
+		media_papel_vendido += planta.vendido();
+		media_papel_reciclado_perdido += planta.reciclado_perdido();
+		media_papel_usado_perdido += planta.usado_perdido();
+		media_falta_papel_reciclado += planta.falta_reciclado();
+	}
+
+	media_papel_vendido /= n_veces;
+	media_papel_reciclado_perdido /= n_veces;
+	media_papel_usado_perdido /= n_veces;
+	media_falta_papel_reciclado /= n_veces;
+
 
 }
