449833e86bf5fd5e749081dfefeb7410408dc976
advy99 <advy99@gmail.com>
2021-01-27 17:46:33 +0100

    variables para estadisticas

--- Ejercicios_teoria/Ejercicio_monte_carlo/include/planta_reciclaje.h
+++ Ejercicios_teoria/Ejercicio_monte_carlo/include/planta_reciclaje.h
@@ -18,6 +18,8 @@ class PlantaReciclaje {
 		double papel_reciclado_perdido = 0.0;
 		double falta_papel_reciclado_vender = 0.0;
 
+		double papel_total_vendido = 0;
+
 		bool avisos = true;
 
 		const double MAX_ROJO = 1000.0;
@@ -39,6 +41,10 @@ class PlantaReciclaje {
 							const double kg_verde, const double papel_dia, const double T_ROJO = 1000.0,
 							const double T_VERDE = 300.0, const double KG_RECICLAR = 3.0);
 		void simular(const bool mensajes_aviso = false, const bool salida_info = false);
+		double vendido();
+		double falta_reciclado();
+		double reciclado_perdido();
+		double usado_perdido();
 
 
 		friend std::ostream & operator << (std::ostream & os, const PlantaReciclaje & planta);
