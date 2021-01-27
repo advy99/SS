2ec02d443e0b1217a95f4cac28879f05797874ea
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-01-27 16:06:08 +0100

    main para la nueva version con repeticiones

--- Ejercicios_teoria/Ejercicio_monte_carlo/src/main.cpp
+++ Ejercicios_teoria/Ejercicio_monte_carlo/src/main.cpp
@@ -3,9 +3,9 @@
 
 int main(int argc, char ** argv) {
 
-	if ( argc != 5 ) {
+	if ( argc != 6 ) {
 		std::cerr << "ERROR: \n"
-					 << "Uso: " << argv[0] << " <num_dias> <kg_rojo> <kg_verde> <kg_reciclado_dia>"
+					 << "Uso: " << argv[0] << " <num_dias> <kg_rojo> <kg_verde> <kg_reciclado_dia> <n_veces>"
 					 << std::endl;
 		exit(-1);
 	}
@@ -15,8 +15,13 @@ int main(int argc, char ** argv) {
 	double kg_verde = atof(argv[3]);
 	double kg_reciclado_dia = atof(argv[4]);
 
+	int n_veces = atoi(argv[5]);
+
 	PlantaReciclaje planta(num_dias, kg_rojo, kg_verde, kg_reciclado_dia);
 
-	planta.simular();
+	// con mensajes de aviso y salida de ejecucion para la ejecucion de prueba
+	planta.simular(true, true);
+
+
 
 }
