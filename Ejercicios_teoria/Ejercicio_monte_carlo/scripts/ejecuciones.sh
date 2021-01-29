03039dc6e603cbed7aee98e7f618527453dab166
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-01-29 20:50:33 +0100

    script de ejecucion actualizado

--- Ejercicios_teoria/Ejercicio_monte_carlo/scripts/ejecuciones.sh
+++ Ejercicios_teoria/Ejercicio_monte_carlo/scripts/ejecuciones.sh
@@ -5,11 +5,12 @@ mkdir -p ../datos
 DIAS=365
 C_ROJO=300
 C_VERDE=0
-KG_PARA_RECICLAR_1=300
+PAPEL_RECICLADO_DIA=300
+VECES=1000000
 
 # Ejecución por defecto, sacamos la salida a un fichero de datos
 # y la salida de error nos mostrará en la terminal si hay algún punto
 # donde se llenan los contenedores
 printf "# Dia actual \t Contenedor rojo \t Contenedor Verde \t Papel usado perdido (sin espacio en rojo) \t Papel reciclado perdido (sin espacio en verde) \t Falta papel vender (sin papel reciclado)\n" > ../datos/ejecucion_defecto.dat
 
-../bin/main_reciclar $DIAS $C_ROJO $C_VERDE $KG_PARA_RECICLAR_1 1 >> ../datos/ejecucion_defecto.dat
+../bin/main_reciclar $DIAS $C_ROJO $C_VERDE $PAPEL_RECICLADO_DIA $VECES
